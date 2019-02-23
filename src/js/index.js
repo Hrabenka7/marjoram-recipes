8e6cc1d9ef71161d871ab4948df0e18dc586ceb4
Marketa <skoloudikovamarketa@gmail.com>
2019-02-23 21:12:36 +0100

    axios requests

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,25 @@
-// Global app controller
-import num from './test' 
+// controller
 
-const x = 23
-console.log(`Imported ${num} from another module, varialbe x is ${x}`)
+import axios from 'axios';
+
+
+async function getRecipes(query) {
+  const proxy = 'https://cors-anywhere.herokuapp.com/'
+  const key = '7893ef9ece0405651bf57db44afdd467'
+  
+  try 
+  {
+    const result = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`)
+    const recipes = result.data.recipes
+    console.log(recipes)
+  }
+  catch (error)
+  {
+    alert(error);
+  }
+}
+
+getRecipes('tomato pasta')
+
+// key 7893ef9ece0405651bf57db44afdd467
+// site  https://www.food2fork.com/api/search
